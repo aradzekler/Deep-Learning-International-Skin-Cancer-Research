softmax activation changed to sigmoid, fixed error

test.py
@@ -110,9 +110,34 @@ def basic_CNN_model():
     return _model
 
 
+def alexNet_model():
+    _model = tf.keras.models.Sequential()
+    _model.add(tf.keras.layers.Conv2D(96, (11, 11), activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)))
+    _model.add(tf.keras.layers.LayerNormalization())
+    _model.add(tf.keras.layers.MaxPooling2D((3, 3)))
+    _model.add(tf.keras.layers.Conv2D(256, (5, 5), activation='relu'))
+    _model.add(tf.keras.layers.LayerNormalization())
+    _model.add(tf.keras.layers.MaxPooling2D((3, 3)))
+    _model.add(tf.keras.layers.Conv2D(384, (3, 3), activation='relu'))
+    _model.add(tf.keras.layers.Conv2D(256, (5, 5), activation='relu'))
+    _model.add(tf.keras.layers.Conv2D(256, (3, 3), activation='relu'))
+    _model.add(tf.keras.layers.MaxPooling2D((3, 3)))
+
+    _model.add(tf.keras.layers.Flatten())
+    _model.add(tf.keras.layers.Dense(4096, activation='relu'))
+    _model.add(tf.keras.layers.Dropout(0.25))
+    _model.add(tf.keras.layers.Dense(1024, activation='relu'))
+    _model.add(tf.keras.layers.Dropout(0.5))
+
+    # softmax classifier
+    _model.add(tf.keras.layers.Dense(7, activation="softmax"))  # 7 features
+    _model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    _model.summary()
+    # return the constructed network architecture
+    return _model
 
 
-def smallerVGGNET_model(num_classes):
+def smallerVGGNET_model():
     _model = tf.keras.models.Sequential()
     # CONV => RELU => POOL
     _model.add(  # padding = "same" results in padding the input such that the output has the same length
@@ -144,19 +169,19 @@ def smallerVGGNET_model(num_classes):
     _model.add(tf.keras.layers.Dropout(0.5))
 
     # softmax classifier
-    _model.add(tf.keras.layers.Dense(num_classes, activation="softmax"))  # 7 features
+    _model.add(tf.keras.layers.Dense(7, activation="softmax"))  # 7 features
     _model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
     _model.summary()
     # return the constructed network architecture
     return _model
 
 
-#model.build(input_shape=(None, IMG_HEIGHT, IMG_WIDTH, 3))
+# model.build(input_shape=(None, IMG_HEIGHT, IMG_WIDTH, 3))
 
-#model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+# model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-#model = smallerVGGNET_model(7).fit_generator(train_data_gen, epochs=30, steps_per_epoch=60)  # train the model
+# model = smallerVGGNET_model(7).fit_generator(train_data_gen, epochs=30, steps_per_epoch=60)  # train the model
 # model = res_net().fit_generator(train_data_gen, epochs=30, steps_per_epoch=30)
 
 model = basic_CNN_model()
@@ -176,13 +201,12 @@ for i in range(len(test_set)):
     img = tf.keras.preprocessing.image.load_img(path=path, target_size=(IMG_HEIGHT, IMG_WIDTH, 3))
     img = tf.keras.preprocessing.image.img_to_array(img)
     test_img = img.reshape((1, IMG_HEIGHT, IMG_WIDTH, 3))
-    #img_class = model.predict(test_img)  # prediction with resnet because model isnot in tf
+    # img_class = model.predict(test_img)  # prediction with resnet because model isnot in tf
     img_class = model.predict_classes(test_img)
     prediction = img_class[0]
     Y_pred.append(prediction)
 
 prediction_classes = [inverted_classes.get(item, item) for item in Y_pred]
-
 
 # prediction variables for outputting predictions
 MEL = []
@@ -207,9 +231,9 @@ pd.DataFrame(predictions).to_excel("D:/Users/arad/ISIC2018T3/predictionss.xlsx",
 history_dict = history.history
 
 acc = history_dict['accuracy']
-#val_acc = history_dict['val_acc']
+# val_acc = history_dict['val_acc']
 loss = history_dict['loss']
-#val_loss = history_dict['val_loss']
+# val_loss = history_dict['val_loss']
 print(history_dict.keys())
 # plotting
 '''